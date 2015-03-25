be55f1b72610a2703ce6531d7708c90ce4082bea
Ben Jones <ben.d.jones@me.com>
2015-03-25 10:08:51 -0700

    Initial Commit

---
+++ iTunesSwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTunesSwiftDemo
+//
+//  Created by Ben Jones on 3/25/15.
+//  Copyright (c) 2015 SocialCode Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
